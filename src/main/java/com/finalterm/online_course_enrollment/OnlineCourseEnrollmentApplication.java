1c492929c9d282b0a5cf362452e77e99525706b7
hoangchitrung <trunghoang1124@gmail.com>
2026-02-08 22:30:15 +0700

    Init project

---
+++ src/main/java/com/finalterm/online_course_enrollment/OnlineCourseEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.finalterm.online_course_enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseEnrollmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalterm/online_course_enrollment/OnlineCourseEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalterm.online_course_enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
