7933a55324a297642212897e6509866f8fec0567
hoangchitrung <trunghoang1124@gmail.com>
2026-02-14 22:33:34 +0700

    Remove MySQL configuration and Dockerfile; update datasource URL in docker-compose and application properties; add User model and repository.

---
+++ src/main/java/com/finalterm/online_course_enrollment/models/User.java
@@ -0,0 +1,9 @@
+package com.finalterm.online_course_enrollment.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+}
---
+++ src/main/java/com/finalterm/online_course_enrollment/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.finalterm.online_course_enrollment.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository{
+    
+}
