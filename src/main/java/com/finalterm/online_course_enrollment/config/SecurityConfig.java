283af12cda1be688546addad4f3ba8c2b408be49
hoangchitrung <trunghoang1124@gmail.com>
2026-03-27 18:33:06 +0700

    refactor: Remove unused imports in SecurityConfig.java for cleaner code

--- src/main/java/com/finalterm/online_course_enrollment/config/SecurityConfig.java
+++ src/main/java/com/finalterm/online_course_enrollment/config/SecurityConfig.java
@@ -2,18 +2,15 @@ package com.finalterm.online_course_enrollment.config;
 
 import com.finalterm.online_course_enrollment.security.JwtAuthenticationFilter;
 import com.finalterm.online_course_enrollment.services.CustomerUserDetailsService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
